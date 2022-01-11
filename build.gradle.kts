48fa68fcea99004be12c3c7a0c7d3bfb46c41017
reviol <taacoder@gmail.com>
2022-01-12 01:12:29 +0300

    dependency for allure added

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,8 @@ dependencies {
     implementation("org.slf4j:slf4j-api:1.7.25")
     implementation("org.slf4j:jcl-over-slf4j:1.7.25")
     testImplementation("org.slf4j:slf4j-log4j12:1.7.25")
+
+    implementation("io.qameta.allure:allure-testng:2.17.2")
 }
 
 tasks.test {
